dc810a31a11b916ff9cff1008e8c12264493e093
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-01-28 16:02:51 +0500

    Refactor styles and main

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express')
+const ejs = require('ejs')
 const app = express()
 
+const Home = require('./controllers/Home')
 const CONFIG = require('./config')
-const ejs = require('ejs')
+const posts = require('./module/data')
+
 
 app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
 
-app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-	res.render('main', {data: 'Ali'})
+	res.render('main', {posts})
+})
+
+
+app.get('/view/:id?', (req, res) => {
+	const { id } = req.params
+	const post = posts.find(d => d.id === (id - 0))
+	res.render('view', post)
 })
 
 app.listen(CONFIG.PORT, () => console.log(`Ready at http://localhost:${CONFIG.PORT}`))
