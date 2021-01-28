b793e2b6e4d87860fbbddf762a489e8a41a31e22
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-01-28 18:38:54 +0500

    Refactor post styles and form styles

--- index.js
+++ index.js
@@ -4,41 +4,55 @@ const app = express()
 
 const Home = require('./controllers/Home')
 const CONFIG = require('./config')
-const posts = require('./module/data')
+// const posts = require('./module/data')
+const { Post } = require('./module/Post')
+const { IO } = require('./module/IO')
+const file = new IO('./module/data.json')
 
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-	res.render('main', {posts})
-})
+;(async () => {
+	const postsFile = await file.read()
+	const posts = JSON.parse(postsFile)
 
-app.get('/view/:id?', (req, res) => {
-	const { id } = req.params
+	app.get('/', (req, res) => {
+		res.render('main', {posts})
+	})
 
-	const post = posts.find(d => d.id === (id - 0))
-	
-	if(Boolean(post)) {
-		res.render('view', post).end()
-	} else {
-		res.status(404)
-		res.redirect('/404').end()
-	}
-})
+	app.get('/view/:id?', (req, res) => {
+		const { id } = req.params
 
-app.get('/create', (req, res) => {
-	res.render('create')
-})
+		const post = posts.find(d => d.id === (id - 0))
+		
+		if(Boolean(post)) {
+			res.render('view', post)
+		} else {
+			res.status(404)
+			res.redirect('/404').end()
+		}
+	})
 
-app.post('/create', (req, res) => {
-	console.log(req.body)
-	res.redirect('/')
-})
+	app.get('/create', (req, res) => {
 
-app.get('/404', (_, res) => {
-	res.render('404')
-})
+		res.render('create')
+	})
+
+	app.post('/create', (req, res) => {
+		const newPost = new Post(req.body)
+		
+		posts.push(newPost)
+		// file.write(JSON.stringify(posts, null, 2))
+
+		res.redirect('/')
+	})
+
+	app.get('/404', (_, res) => {
+		res.render('404')
+	})
+
+})() //end of async
 
 app.listen(CONFIG.PORT, () => console.log(`Ready at http://localhost:${CONFIG.PORT}`))
