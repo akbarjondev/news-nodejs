b232d9b000fb823a1732a088310a694dc39fc6a0
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-01-28 16:02:33 +0500

    Make header component

---
+++ module/data.js
@@ -0,0 +1,22 @@
+const posts = [
+	{
+		id: 1,
+		title: 'Title 1',
+		text: 'This is post text',
+		date: '28-01-2021'
+	},
+	{
+		id: 2,
+		title: 'Title 2',
+		text: 'This is post text',
+		date: '28-01-2021'
+	},
+	{
+		id: 3,
+		title: 'Title 3',
+		text: 'This is post text',
+		date: '28-01-2021'
+	},
+]
+
+module.exports = posts
